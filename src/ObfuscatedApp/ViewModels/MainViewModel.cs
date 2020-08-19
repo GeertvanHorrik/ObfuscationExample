4f436fa464ff6b9b5927741d8f400d95fe3120a3
Geert van Horrik <geert@catenalogic.com>
2020-08-19 09:58:44 +0200

    Build initial app

---
+++ src/ObfuscatedApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ObfuscatedApp.ViewModels
+{
+    using Catel.MVVM;
+    using System.Threading.Tasks;
+
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            FirstName = "John";
+            LastName = "Doe";
+        }
+
+        public override string Title { get { return "Welcome to Obfuscated App"; } }
+
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
